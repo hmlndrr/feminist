bf69cf3e93482e2656f370ef89cc9b2c777a9bdb
Nie-Mand <chill.out.x19@gmail.com>
2023-02-02 20:40:40 +0100

    permissions

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-chmod 400 shefiles
-chmod 400 templates
-chmod 400 .gitignore
-chmod 400 requirements.txt
-chmod 400 feminist.py
-chmod 500 run.sh
+chmod 555 -R shefiles
+chmod 555 -R templates
+chmod 555 .gitignore
+chmod 555 requirements.txt
+chmod 555 feminist.py
+chmod 555 run.sh
 gunicorn feminist:app
